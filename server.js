febe1ad21f97ba2f50ec92606f28d8d99db7970d
Pradeenaik <pradeepnaik390@gmail.com>
2026-05-12 07:27:32 +0530

    fixed bug

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://coastal-lodge-frontend.vercel.app/",
+      "https://coastal-lodge-frontend.vercel.app",
     ],
     credentials: true,
   })
